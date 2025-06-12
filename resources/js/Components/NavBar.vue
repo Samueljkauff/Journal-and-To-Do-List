60d539ebe1dee0ef79ddadc6248235d1909d5faf
samueljkauff <samueljkauff@gmail.com>
2025-06-12 00:51:22 -0400

    Welcome page finished

--- resources/js/Components/NavBar.vue
+++ resources/js/Components/NavBar.vue
@@ -8,37 +8,72 @@
             >
                 <i class="fas fa-bars text-2xl"></i>
             </div>
-            <div
-                class="w-full flex justify-center items-center space-x-2 border-t border-slate-700 py-4 hover:bg-gradient-to-r hover:from-purple-800 hover:to-indigo-800 hover:bg-slate-700"
-                v-if="onNavClick"
+            <transition-group
+                name="fall"
+                tag="div"
+                class="flex flex-col"
             >
-                <i class="fas fa-home inline" />
-                <p class="inline">Home</p>
-            </div>
-            <div
-                class="w-full flex justify-center items-center space-x-2 border-t border-slate-700 py-4 hover:bg-gradient-to-r hover:from-purple-800 hover:to-indigo-800 hover:bg-slate-700"
-                v-if="onNavClick"
-            >
-                <i class="fas fa-info-circle inline" />
-                <p class="inline">About</p>
-            </div>
-            <div
-                class="w-full flex justify-center items-center space-x-2 border-t border-slate-700 py-4 hover:bg-gradient-to-r hover:from-purple-800 hover:to-indigo-800 hover:bg-slate-700"
-                v-if="onNavClick"
-            >
-                <i class="fas fa-user inline" />
-                <p class="inline">Profile</p>
-            </div>
-            <div
-                class="w-full flex justify-center items-center space-x-2 border-t border-slate-700 py-4 hover:bg-gradient-to-r hover:from-purple-800 hover:to-indigo-800 hover:bg-slate-700"
-                v-if="onNavClick"
-            >
-                <i class="fas fa-cog inline" />
-                <p class="inline">Settings</p>
-            </div>
+                <div
+                    v-if="onNavClick"
+                    key="home"
+                    class="w-full flex justify-center items-center space-x-2 border-t border-slate-700 py-4 hover:bg-gradient-to-r hover:from-slate-900 hover:to-slate-700 hover:bg-slate-700"
+                >
+                    <i class="fas fa-home inline" />
+                    <p class="inline">Home</p>
+                </div>
+                <div
+                    v-if="onNavClick"
+                    key="about"
+                    class="w-full flex justify-center items-center space-x-2 border-t border-slate-700 py-4 hover:bg-gradient-to-r hover:from-slate-900 hover:to-slate-700 hover:bg-slate-700"
+                >
+                    <i class="fas fa-info-circle inline" />
+                    <p class="inline">About</p>
+                </div>
+                <div
+                    v-if="onNavClick"
+                    key="profile"
+                    class="w-full flex justify-center items-center space-x-2 border-t border-slate-700 py-4 hover:bg-gradient-to-r hover:from-slate-900 hover:to-slate-700 hover:bg-slate-700"
+                >
+                    <i class="fas fa-user inline" />
+                    <p class="inline">Profile</p>
+                </div>
+                <div
+                    v-if="onNavClick"
+                    key="settings"
+                    class="w-full flex justify-center items-center space-x-2 border-t border-slate-700 py-4 hover:bg-gradient-to-r hover:from-slate-900 hover:to-slate-700 hover:bg-slate-700"
+                >
+                    <i class="fas fa-cog inline" />
+                    <p class="inline">Settings</p>
+                </div>
+            </transition-group>
         </nav>
     </div>
 </template>
+
+<style scoped>
+.fall-enter-active {
+  transition: all 0.5s cubic-bezier(0.23, 1, 0.32, 1);
+}
+.fall-leave-active {
+  transition: all 1s cubic-bezier(0.23, 1, 0.32, 1);
+}
+.fall-enter-from {
+  opacity: 0;
+  transform: translateY(-40px) scaleY(0.7);
+}
+.fall-enter-to {
+  opacity: 1;
+  transform: translateY(0) scaleY(1);
+}
+.fall-leave-from {
+  opacity: 1;
+  transform: translateY(0) scaleY(1);
+}
+.fall-leave-to {
+  opacity: 0;
+  transform: translateX(-200px) scaleY(0.7);
+}
+</style>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -4,6 +4,15 @@
     <div class="min-h-screen flex items-center justify-center bg-slate-800">
         <div class="flex justify-start items-start w-full h-full">
             <NavBar />
+            <div class="w-full h-screen m-4 bg-slate-900 rounded-xl shadow-lg p-6">
+                <div class="flex flex-col items-center justify-center h-full">
+                    <i class="fas fa-calendar-days text-6xl text-green-500 mb-4"></i>
+                    <h1 class="text-3xl text-white mb-4">Welcome to the TooDoo</h1>
+                    <p class="text-gray-300 size">This is a simple application to keep track of your life!</p>
+                    <p class="text-gray-500 text-sm mt-2">Click the Three bars to start keeping track of your past Present and Future.</p>
+                </div>
+
+            </div>
         </div>
     </div>
 </template>
