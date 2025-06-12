04cce665a8cca38810216c730c61f0782146fbf1
samueljkauff <samueljkauff@gmail.com>
2025-06-12 00:09:38 -0400

    background added

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,13 +1,12 @@
 <template>
-
     <Head title="Welcome!" />
 
-    <div class="text-white min-h-screen flex items-center justify-center">
+    <div class="min-h-screen flex items-center justify-center bg-slate-800">
         <div class="flex justify-start items-start w-full h-full">
             <NavBar />
         </div>
     </div>
- </template>
+</template>
 
 <script setup lang="ts">
 import NavBar from '@/Components/NavBar.vue';
