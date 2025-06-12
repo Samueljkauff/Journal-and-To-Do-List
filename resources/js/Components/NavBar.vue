fcc8d37bc624c0246b1d2231227ab87b7d2c87ba
samueljkauff <samueljkauff@gmail.com>
2025-06-11 23:57:45 -0400

    Nav bar style finished for not

--- resources/js/Components/NavBar.vue
+++ resources/js/Components/NavBar.vue
@@ -1,38 +1,38 @@
 <template>
     <div class="text-white min-h-screen flex items-center justify-center">
-        <nav :class="{ 'w-40': logoClicked, 'w-16': !logoClicked }"
+        <nav :class="{ 'w-40': onNavClick, 'w-16': !onNavClick }"
             class="flex flex-col bg-slate-900 h-screen w-16 transition-all duration-300">
-            <i class="fas fa-bars text-2xl my-3 mx-auto hover:cursor-pointer" @click="onLogoClick"></i>
-            <hr class="border-t border-slate-700 my-2 mx-4" v-if="logoClicked" />
             <div
-                class="mx-auto flex items-center space-x-2 border-t border-slate-900 py-4"
-                v-if="logoClicked"
+                @click="onLogoClick"
+                class="flex justify-center py-4 hover:bg-gradient-to-r hover:from-slate-900 hover:to-slate-700 hover:cursor-pointer"
+            >
+                <i class="fas fa-bars text-2xl"></i>
+            </div>
+            <div
+                class="w-full flex justify-center items-center space-x-2 border-t border-slate-700 py-4 hover:bg-gradient-to-r hover:from-purple-800 hover:to-indigo-800 hover:bg-slate-700"
+                v-if="onNavClick"
             >
                 <i class="fas fa-home inline" />
                 <p class="inline">Home</p>
             </div>
-            <hr class="border-t border-slate-700 my-2 mx-4" v-if="logoClicked" />
             <div
-                class="mx-auto flex items-center space-x-2 py-4"
-                v-if="logoClicked"
+                class="w-full flex justify-center items-center space-x-2 border-t border-slate-700 py-4 hover:bg-gradient-to-r hover:from-purple-800 hover:to-indigo-800 hover:bg-slate-700"
+                v-if="onNavClick"
             >
                 <i class="fas fa-info-circle inline" />
                 <p class="inline">About</p>
             </div>
-            <hr class="border-t border-slate-700 my-2 mx-4" v-if="logoClicked" />
             <div
-                class="mx-auto flex items-center space-x-2 py-4"
-                v-if="logoClicked"
+                class="w-full flex justify-center items-center space-x-2 border-t border-slate-700 py-4 hover:bg-gradient-to-r hover:from-purple-800 hover:to-indigo-800 hover:bg-slate-700"
+                v-if="onNavClick"
             >
                 <i class="fas fa-user inline" />
                 <p class="inline">Profile</p>
             </div>
-            <hr class="border-t border-slate-700 my-2 mx-4" v-if="logoClicked" />
             <div
-                class="mx-auto flex items-center space-x-2 py-4"
-                v-if="logoClicked"
+                class="w-full flex justify-center items-center space-x-2 border-t border-slate-700 py-4 hover:bg-gradient-to-r hover:from-purple-800 hover:to-indigo-800 hover:bg-slate-700"
+                v-if="onNavClick"
             >
-                
                 <i class="fas fa-cog inline" />
                 <p class="inline">Settings</p>
             </div>
@@ -48,13 +48,13 @@ export default defineComponent({
     name: 'NavBar',
     data() {
         return {
-            logoClicked: false,
+            onNavClick: false,
         };
     },
     methods: {
         onLogoClick() {
-            this.logoClicked = !this.logoClicked;
-            console.log('Logo clicked:', this.logoClicked);
+            this.onNavClick = !this.onNavClick;
+            console.log('Logo clicked:', this.onNavClick);
         }
     }
 });
