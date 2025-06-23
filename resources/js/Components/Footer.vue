2733fd0997c4cfa45afd91e446633c88e11bc1fd
samueljkauff <samueljkauff@gmail.com>
2025-06-22 23:18:49 -0400

    Refined footer

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -1,29 +1,55 @@
 <template>
     <footer class="fixed bottom-0 left-0 w-full">
-        <div :class="{'h-24': clicked}" class="flex justify-center items-center w-full bg-slate-800 h-12 text-white">
-            <div  @click="onClick" class="flex items-center justify-center w-8 h-8 rounded-full hover:bg-slate-700 transition-colors duration-300 cursor-pointer">
-            <i v-if="!clicked" class="fas fa-plus"></i>
-            <i v-else class="fas fa-minus"></i>
-            <p v-else >Github</p>
-            </div>
+        <div
+            :class="{'h-24': clicked, 'h-12': !clicked}"
+            class="flex justify-end pt-1 pr-[1rem] w-full bg-slate-800 text-white transition-all duration-300"
+        >
+            <transition name="icon-container-slide">
+                <div
+                    v-if="true"
+                    key="icon-container"
+                    @click="onClick"
+                    class="flex items-center justify-center w-8 h-8 rounded-full hover:bg-slate-700 transition-colors duration-300 cursor-pointer"
+                >
+                    <span>
+                        <i v-if="!clicked" class="fas fa-plus"></i>
+                        <i v-else class="fas fa-minus"></i>
+                    </span>
+                </div>
+            </transition>
         </div>
     </footer>
 </template>
 
-<script lang="ts">
-    export default {
-        name: 'Footer',
+<style scoped>
+.footer-slide-enter-active,
+.footer-slide-leave-active {
+  transition: all 0.3s cubic-bezier(0.4, 0, 0.2, 1);
+}
+.footer-slide-enter-from,
+.footer-slide-leave-to {
+  opacity: 0;
+  transform: translateY(2rem);
+}
+.footer-slide-enter-to,
+.footer-slide-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+</style>
 
-        data() {
-            return {
-                clicked: false,
-            };
-        },
-        methods: {
-            onClick() {
-                this.clicked = !this.clicked;
-                // You can add more functionality here if needed
-            }
+<script lang="ts">
+export default {
+    name: 'Footer',
+    data() {
+        return {
+            clicked: false,
+        };
+    },
+    methods: {
+        onClick() {
+            this.clicked = !this.clicked;
         }
-    };
+    }
+};
 </script>
